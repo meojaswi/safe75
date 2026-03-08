b5f8a21c265b4d153e6db906587d2ec58c139c8d
meojaswi <ojaswi3236@gmail.com>
2026-03-08 15:37:37 +0530

    Update server.js for attendance system

--- server/server.js
+++ server/server.js
@@ -4,13 +4,14 @@ require("dotenv").config();
 
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
-
+const attendanceRoutes = require("./routes/attendanceRoutes");
 const app = express();
 const subjectRoutes = require("./routes/subjectRoutes");
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/subjects", subjectRoutes);
-
+app.use("/api/attendance", attendanceRoutes);
 connectDB();
 
 app.use("/api/auth", authRoutes);
