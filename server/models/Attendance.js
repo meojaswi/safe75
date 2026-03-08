2bed8159c242c77d5b1aa46fb1a4af46e5772734
meojaswi <ojaswi3236@gmail.com>
2026-03-08 15:29:16 +0530

    Update server/models/Attendance.js

---
+++ server/models/Attendance.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const attendanceSchema = new mongoose.Schema({
+  subjectId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Subject",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["present", "absent"],
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Attendance", attendanceSchema);
