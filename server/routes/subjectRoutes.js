a032361117af68af61ab2a861b8baf5d792535a1
meojaswi <ojaswi3236@gmail.com>
2026-03-08 15:22:16 +0530

    Update server/routes/subjectRoutes.js

--- server/routes/subjectRoutes.js
+++ server/routes/subjectRoutes.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { addSubject } = require("../controllers/subjectController");
-
-router.post("/", addSubject);
-
-module.exports = router;
 const { addSubject, getSubjects } = require("../controllers/subjectController");
 
 router.post("/", addSubject);
 router.get("/", getSubjects);
+
+module.exports = router;
