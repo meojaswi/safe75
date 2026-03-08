97801470bc4c442b5ac157ded3abdc75fd88c042
meojaswi <ojaswi3236@gmail.com>
2026-03-08 14:05:10 +0530

    Update server/server.js

--- server/server.js
+++ server/server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Safe75 API running");
