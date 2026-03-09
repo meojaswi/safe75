cb82ac7e73d064bd8444a34da9041369d6ef9c12
meojaswi <ojaswi3236@gmail.com>
2026-03-09 13:53:35 +0530

    Update Navbar to Sidebar

---
+++ client/js/sidebar.js
@@ -0,0 +1,5 @@
+// Sidebar toggle for mobile
+function toggleSidebar() {
+  document.getElementById("sidebar").classList.toggle("open");
+  document.getElementById("sidebarOverlay").classList.toggle("open");
+}
